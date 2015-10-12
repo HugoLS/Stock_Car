c7bb36ad117985a36bc0cad541a954773e753082
Hugo <hugo.leray.scheidt@gmail.com>
2015-10-12 22:00:14 +0200

    Avance vers la seconde phase
    -Manette freebox fonctionnelle mais pas celle du pack média
    -La voiture tourne correctement et est orienté correctement
    -Placement automatique de la voiture
    -Collision du terrain en place
    -Gameover en place
    -Bouton de restart

--- index.js
+++ index.js
@@ -5,16 +5,26 @@ var car = require('./car.js');
 
 var connectedPlayers = 0;
 var cars = {};
-//console.log(carInstance.color);
+var controls = {};
+
+var mapWidth = 500;
+var mapHeight = 500;
+var carWidth = 10;
+var carHeight = 20;
+
+var maxPositionValueX = mapWidth - carWidth;
+var maxPositionValueY = mapHeight - carHeight;
+
+var minPositionValueX = carWidth;
+var minPositionValueY = carHeight; 
 
 /* ****************************************************************************** */
 /* ******************************* ROUTER *************************************** */
 /* ****************************************************************************** */
 
 app.get('/', function(req, res){
-  res.sendFile('/var/www/StockCar/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
-
 
 /* ****************************************************************************** */
 /* ******************************* SOCKETS ************************************** */
@@ -22,23 +32,25 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   connectedPlayers++;
-  var carInstance = new car();
-  carInstance.id = socket.id;
-  cars[carInstance.id] = carInstance;
-  carInstance.color = getRandomColor();
+  console.log(connectedPlayers);
 
-  //console.log(carInstance.color);
+  socket.on('launchGame', function(msg){
+    var newCarInstance = new car();
+    initCar(newCarInstance, socket);
 
-  socket.on('newGame', function(msg){
-    socket.emit('newGame', carInstance.color);
+    cars[newCarInstance.id] = newCarInstance;
+
+    socket.emit('newGame', JSON.stringify(newCarInstance));
   });
 
-  socket.on('changeColor', function(msg){
-    socket.emit('changeColor', "red");
-  });  
-  
+  socket.on('updateControls', function(msg){
+    controls[socket.id] = JSON.parse(msg);
+  });
+
   socket.on('disconnect', function(){
     connectedPlayers--;
+    delete cars[socket.id];
+    delete controls[socket.id];
   	console.log(connectedPlayers);
   });
 });
@@ -58,4 +70,75 @@ function getRandomColor() {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
+}
+
+function initCar(CarInstance,socket) {
+  ux = 1;
+  uy = 0;
+  CarInstance.id = socket.id;
+  CarInstance.color = getRandomColor();
+  CarInstance.width = carWidth;
+  CarInstance.height = carHeight;
+  CarInstance.x = Math.floor(Math.random() * (maxPositionValueX - minPositionValueX)) + minPositionValueX;
+  CarInstance.y = Math.floor(Math.random() * (maxPositionValueY - minPositionValueY)) + minPositionValueY;
+  CarInstance.vx = (mapWidth / 2) - CarInstance.x;
+  CarInstance.vy = (mapHeight / 2) - CarInstance.y;
+  vx = CarInstance.vx;
+  vy = CarInstance.vy;
+  u_scal_v = ux * vx + uy * vy;
+  norm_u = Math.sqrt(Math.pow(ux,2)+Math.pow(uy,2));
+  norm_v = Math.sqrt(Math.pow(vx,2)+Math.pow(vy,2));
+  angle = u_scal_v/(norm_u*norm_v);
+  if(vy < 0)
+  {
+    angle = -angle;
+  }
+  CarInstance.angle = Math.acos(angle);
+}
+
+
+/* ****************************************************************************** */
+/* ******************************* GAME LOOP ************************************ */
+/* ****************************************************************************** */
+
+var updateloop = setInterval(function () {
+        update();
+    }, 40);
+
+function update() {
+  //console.log(JSON.stringify(cars));
+  for (var idCar in cars){
+    if(controls.hasOwnProperty(idCar)){
+
+      var angle = 0;
+      var speed = 6;
+
+      if(controls[idCar].left == 1){
+        angle -= Math.PI / 36;
+      }
+      if(controls[idCar].right == 1){
+        angle += Math.PI / 36;
+      }
+      cars[idCar].angle += angle;
+      normv = Math.sqrt(cars[idCar].vx * cars[idCar].vx + cars[idCar].vy * cars[idCar].vy);
+
+      cars[idCar].vx = cars[idCar].vx * Math.cos(angle) - cars[idCar].vy * Math.sin(angle);
+      cars[idCar].vy = cars[idCar].vx * Math.sin(angle) + cars[idCar].vy * Math.cos(angle);
+
+      cars[idCar].vx = cars[idCar].vx / normv;
+      cars[idCar].vy = cars[idCar].vy / normv;
+
+      cars[idCar].x +=  speed * cars[idCar].vx;
+      cars[idCar].y +=  speed * cars[idCar].vy;
+
+      if(cars[idCar].x > mapWidth | cars[idCar].y > mapHeight |  cars[idCar].x < 0 | cars[idCar].y < 0){
+          io.sockets.connected[idCar].emit('gameOver', 'dead!');
+          delete cars[idCar];
+          delete controls[idCar];
+      }
+      //console.log(cars[idCar].vx);
+    }
+  }
+
+  io.emit('update', JSON.stringify(cars));
 }
